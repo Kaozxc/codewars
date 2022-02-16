9d88e63fea6d3e61cbb5935e0f4be499a8f34e23
Kaoz <kaoz415@gmail.com>
2022-02-16 16:08:42 +0100

    next

---
+++ 7kyuStrayNumber.js
@@ -0,0 +1,16 @@
+function stray(numbers) {
+    let strayChar = numbers[0];
+    
+    if(strayChar !== numbers[1] && strayChar !== numbers[2]) {
+      return strayChar;
+    }
+    
+    for(let i = 1; i < numbers.length; i++) {
+      if(strayChar !== numbers[i]) {
+        return numbers[i]
+        }
+    }
+  }
+  
+  // Solution 1:  // return numbers = numbers.reduce((a, b) => a ^ b);
+  
