366080ba07f45b144b6dfb62a715a8ca4477e4ac
Kaoz <kaoz415@gmail.com>
2022-02-16 21:01:49 +0100

    new

--- 7kyuStrayNumber.js
+++ 7kyuStrayNumber.js
@@ -1,3 +1,17 @@
+// Instructions:
+
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+// Complete the method which accepts such an array, and returns that single different number.
+
+// The input array will always be valid! (odd-length >= 3)
+// Examples
+
+// [1, 1, 2] ==> 2
+// [17, 17, 3, 17, 17, 17, 17] ==> 3
+
+
+
 function stray(numbers) {
     let strayChar = numbers[0];
     
@@ -13,4 +27,3 @@ function stray(numbers) {
   }
   
   // Solution 1:  // return numbers = numbers.reduce((a, b) => a ^ b);
-  
